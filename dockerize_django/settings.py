2a2d0baf633248ef4ea6b808aac97307c93de098
ashrafkvt <ashrafkvt6197@gmail.com>
2023-11-10 00:02:56 +0530

    do initial setup

--- dockerize_django/settings.py
+++ dockerize_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&)ooq1s_fee-)4nop-bj8815hd#^(u1g#l^0vlzfu_io@qoo8r'
+SECRET_KEY = 'django-insecure-_k&*tizwro+vt^n%k2#i4a4jm9l*1#q@s@u1-*6kx(yff-wd+3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
